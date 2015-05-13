e90d35f7ccd2fd28cda5284a12a37a54cc201821
Francisco Cano Marchal <fcanomar@gmail.com>
2015-05-13 19:38:27 +0100

    creados archivos de seguridad: responsable y usuario

--- __openerp__.py
+++ __openerp__.py
@@ -6,7 +6,7 @@ Añade los campos necesarios para la base de datos de productos de laboratorio
 """,
 'author' : 'Francisco Cano Marchal',
 'depends' : ['base','purchase'],
-'data' : ['dblaboratorio_view.xml','marcas_menu.xml','sequences.xml','caducidad_scheduler.xml'],
+'data' : ['dblaboratorio_view.xml','marcas_menu.xml','sequences.xml','caducidad_scheduler.xml','security/dblabo_security.xml','security/ir.model.access.csv'],
 'demo': [],
 'installable' : True,
 'auto_install' : False,
