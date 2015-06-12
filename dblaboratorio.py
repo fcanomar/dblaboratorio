888345f34d650b57473b05ab259dba2e8e4a12d8
Francisco Cano Marchal <fcanomar@gmail.com>
2015-06-12 11:06:38 +0100

    codigo refactorizado. fuera x_calidadfab calidadadcm etc y calculo de fechas a partir de x_caducidad en product.template

--- dblaboratorio.py
+++ dblaboratorio.py
@@ -9,13 +9,6 @@ from datetime import date
 
 
 
-class dblaboratorio_reactivo_quant (models.Model):
-    _inherit = "stock.quant"
-    
-    x_caducidad = fields.Date('Fecha de Caducidad')
-    
-    
-
 class dblaboratorio_product_template (models.Model) :
     _inherit = "product.template"
     
@@ -41,17 +34,6 @@ class dblaboratorio_product_template (models.Model) :
         self.enviar_mensajes_caducidad_reactivos(30)
         self.enviar_mensajes_caducidad_reactivos(7)
         self.enviar_mensajes_caducidad_reactivos(1)
-          
-    @api.multi
-    def action_get_codigo_reactivo(self):
-        #self.enviar_mensajes_caducidad_reactivos()
-        self.default_code = self._get_codigo()
-    
-    @api.multi
-    def _get_codigo(self):
-        #if self.x_tipolabo == 'reactivo':
-            #print seq
-            return self.env['ir.sequence'].next_by_code('react')
               
     
     @api.model
@@ -64,48 +46,14 @@ class dblaboratorio_product_template (models.Model) :
     
     x_tipolabo = fields.Selection([('reactivo','Reactivo'),('materiallabo','Material de Laboratorio'),('materialrefe','Material de Referencia'),('patron','Patron'),('equipo','Equipo'),('generico','Genérico')],'Clase de Producto',default='generico') 
     x_marca = fields.Many2one('dblaboratorio.marca','Marca', ondelete='cascade')
-    x_calidadfabr = fields.Many2one('dblaboratorio.calidadesmarca','Calidad Fabricante', ondelete='cascade', domain="[('x_marca','=',x_marca)]")
-    x_calidadcm = fields.Many2one('dblaboratorio.calidadcm','Cumple Especificaciones CM', ondelete='cascade', domain="[('x_eqespec','=',x_calidadfabr),('x_marca','=',x_marca)]") 
     x_formato = fields.Many2one('dblaboratorio.formato', 'Formato', ondelete='cascade')
     x_conservacion = fields.Many2one('dblaboratorio.conservacion', 'Conservacion', ondelete='cascade',domain="[('name','=',x_espreact)]", related="x_espreact.x_conservacion", readonly=True)
-    x_caducidad = fields.Date('Fecha de Caducidad')
-    x_today = fields.Date('Today', compute='_get_today')
-    x_time = fields.Char('Time', compute='_get_time')
-    x_trestante = fields.Integer('Caducidad en Meses', compute='_get_trestante')
-    x_daysrestante = fields.Integer('Caducidad en Dias', compute='_get_daysrestante')
     x_estado = fields.Selection([('solido','Solido'),('liquido','Liquido'),('gaseoso','Gaseoso')],'Estado', domain="[('name','=',x_espreact)]", related="x_espreact.x_estado", readonly=True)
     x_tipocodigo = fields.Selection([('ean13','EAN13'),('qweb','Qweb')],'Tipo de Codigo')
     x_qweb = fields.Char('Codigo Qweb')
     x_espreact = fields.Many2one('dblaboratorio.reactivoesp','Cumple Especificaciones',ondelete='cascade')
     x_nri = fields.Char('NRI', related='x_espreact.x_nri', readonly=True)
-    #x_secuenciaprod = fields.Many2one('ir.sequence','Secuencia producto', ondelete='cascade')
-    #x_codigo = fields.Char('Referencia de Laboratorio')
-    
-    #material de referencia
-    
    
-   
-    @api.one
-    def _get_today(self):
-        self.x_today = date.today()
-    
-    @api.one
-    def _get_time(self):
-        self.x_time = datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')
-        #print('hora!')
-     
-    @api.depends('x_caducidad','x_today')
-    def _get_trestante(self):
-        for record in self:
-            days = record.x_caducidad and (fields.Date.from_string(record.x_caducidad) - fields.Date.from_string(fields.Date.today())).days
-            record.x_trestante = days/30    
-                  
-    @api.depends('x_caducidad','x_today')
-    def _get_daysrestante(self):
-        for record in self:
-            days = record.x_caducidad and (fields.Date.from_string(record.x_caducidad) - fields.Date.from_string(fields.Date.today())).days
-            record.x_daysrestante = days 
-
     _sql_constraints = [
     ('codigo_unico','UNIQUE(x_codigo)',"La referencia que pretende utilizar pertenece a un producto ya existente."),]       
 
