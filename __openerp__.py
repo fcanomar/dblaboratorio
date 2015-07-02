7d4d75961731f1f146a6be49d16ad9f985a1dc1a
Francisco Cano Marchal <fcanomar@gmail.com>
2015-07-02 21:09:52 +0100

    añadido procurement.xml a medias para rellenar la ubicacion destino de preparar en laboratorio desde el archivo xml. quitados este y el archivo de configuraciones dblaboratorio_data.xml de openerp.py

--- __openerp__.py
+++ __openerp__.py
@@ -6,7 +6,7 @@ Añade los campos necesarios para la base de datos de productos de laboratorio
 """,
 'author' : 'Francisco Cano Marchal',
 'depends' : ['base','purchase','administracioncm','product_expiry'],
-'data' : ['security/dblabo_security.xml','security/ir.model.access.csv','dblaboratorio_view.xml','laboratorio_view.xml','sequences.xml','caducidad_scheduler.xml','categories_data.xml','dblaboratorio_data.xml'],
+'data' : ['security/dblabo_security.xml','security/ir.model.access.csv','dblaboratorio_view.xml','laboratorio_view.xml','sequences.xml','caducidad_scheduler.xml','categories_data.xml'],
 'demo': [],
 'installable' : True,
 'auto_install' : False,
