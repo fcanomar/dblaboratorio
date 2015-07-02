abf560c059327b1c1f0fab870139352b07c1fd1b
Francisco Cano Marchal <fcanomar@gmail.com>
2015-07-02 11:11:28 +0100

    añadidas rutas a categorias. añadidas dependencias. quitado datos de la empresa y movido a administracioncm

--- __openerp__.py
+++ __openerp__.py
@@ -5,8 +5,8 @@
 Añade los campos necesarios para la base de datos de productos de laboratorio
 """,
 'author' : 'Francisco Cano Marchal',
-'depends' : ['base','purchase'],
-'data' : ['dblaboratorio_view.xml','laboratorio_view.xml','sequences.xml','caducidad_scheduler.xml','security/dblabo_security.xml','security/ir.model.access.csv','categories_data.xml','dblaboratorio_data.xml'],
+'depends' : ['base','purchase','administracioncm','product_expiry'],
+'data' : ['security/dblabo_security.xml','security/ir.model.access.csv','dblaboratorio_view.xml','laboratorio_view.xml','sequences.xml','caducidad_scheduler.xml','categories_data.xml','dblaboratorio_data.xml'],
 'demo': [],
 'installable' : True,
 'auto_install' : False,
