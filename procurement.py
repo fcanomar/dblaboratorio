dcb8de296f7ca3795f31df0748d2a242318fd24e
Francisco Cano Marchal <fcanomar@gmail.com>
2016-05-24 12:38:24 +0100

    añade laboratorio como ubicacion de abastecimiento por defecto a disoluciones

--- procurement.py
+++ procurement.py
@@ -6,13 +6,15 @@ from openerp import models, api, fields
 class dblaboratorio_procurement_order (models.Model) :
     _inherit = "procurement.order"
     
-    @api.onchange('product_qty')
+    @api.multi
+    @api.depends('product_id.x_tipolabo')
     def _get_procurement_location(self):
         
         if self.product_id.x_tipolabo == 'disolucion':
             self.location_id = self.env['stock.location'].search([('name','=','Laboratorio')]);
         
-    
+
+    location_id = fields.Many2one('stock.location', 'Procurement Location', compute=_get_procurement_location)
 #    product_id = fields.Many2one('product.product', 'Product', required=True, states={'confirmed': [('readonly', False)]}, readonly=True, default='_get_procurement_location'),
     
 
